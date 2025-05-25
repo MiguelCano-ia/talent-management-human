8a7ed4e0fc8abdfe30e3665899bf38013d9d9578
Luis Alejandro Giraldo Bolaños <luis.giraldo3@utp.edu.co>
2025-05-25 15:09:23 -0500

    merge

--- src/features/auth/services/auth.service.ts
+++ src/features/auth/services/auth.service.ts
@@ -7,7 +7,7 @@ import { BranchOfOffice } from "../interfaces/branchOfOffice.interface";
 import { MeansOfPayment } from "../interfaces/meansOfPayment.interface";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/v1/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api/v1/",
   headers: {
     "Content-Type": "application/json",
   },
